08861db2232774abcbeaddc2a31cd1c979e4f3b0
jairorjuela <genius85862@gmail.com>
2019-06-22 13:13:02 -0500

    Change for Hooks

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      text: " "
-    }
-  }
-  render() {
-    return (
-      <div>
-        <h1>Un repetidor</h1>
-        <input type="text" onChange={this.handleChange.bind(this)} placeholder="Empieza a escribir algo" />
-        <p className="repeater">{this.state.text}</p>
-      </div>
-    );
-  }
+export const App = () => {
+  const [text, setText] = useState("");
 
-  handleChange(event){
-    var texto = event.target.value
-    this.setState({
-      text: texto
-    });
-  }
-}
+  return (
+    <div>
+      <h1>Un repetidor</h1>
+      <input type="text" onChange={e => setText(e.target.value)} placeholder="Empieza a escribir algo" />
+      <p className="repeater">{text}</p>
+    </div>
+  );
+};
 
 export default App;
